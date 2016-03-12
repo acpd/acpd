b3eabcb13f1d621f0e6693cd4a6ec5dad5f98500
Jonathan Helmus <jjhelmus@gmail.com>
2016-03-12 14:53:44 -0600

    Script to store the source dirs in the cache dir

---
+++ scripts/cache_src_dirs.sh
@@ -0,0 +1,4 @@
+# Store in the cache directory the conda-bld source directories
+mkdir -p /acpd/cache
+cp -rf /opt/conda/conda-bld/src_cache /acpd/cache/
+cp -rf /opt/conda/conda-bld/git_cache /acpd/cache/
