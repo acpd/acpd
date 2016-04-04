f1bebf44b6df14ca52387d34f0fc21bcda672132
Jonathan Helmus <jjhelmus@gmail.com>
2016-04-04 11:29:13 -0600

    WIP: OS X openssl build in recipe

--- recipes/openssl/build.sh
+++ recipes/openssl/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-./config shared --prefix=$PREFIX
+
+if [ `uname` == Darwin ]; then
+    ./Configure darwin64-x86_64-cc shared --prefix=$PREFIX
+    make depend
+else
+    ./config shared --prefix=$PREFIX
+fi
 make
 make install
+
+# remove man pages
+rm -rf $PREFIX/ssl/man
